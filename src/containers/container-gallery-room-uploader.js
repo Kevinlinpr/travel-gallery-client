f2efdca7f494f1fa826c38c876ca29a960446c2a
Kevinlinpr <kevinlinpr@gmail.com>
2019-01-06 10:44:00 +0800

    successfully get response from server after uploading file

--- src/containers/container-gallery-room-uploader.js
+++ src/containers/container-gallery-room-uploader.js
@@ -1,43 +1,31 @@
 import React from 'react';
-import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
 import DialogContent from '@material-ui/core/DialogContent';
 import DialogContentText from '@material-ui/core/DialogContentText';
 import DialogTitle from '@material-ui/core/DialogTitle';
 import Slide from '@material-ui/core/Slide';
 import {bindActionCreators} from "redux";
 import {openUploader,closeUploader} from "../actions";
-// Import React FilePond
-import { FilePond, File, registerPlugin } from 'react-filepond';
-// Import FilePond styles
+import { FilePond} from 'react-filepond';
 import 'filepond/dist/filepond.min.css';
-// Import the Image EXIF Orientation and Image Preview plugins
-// Note: These need to be installed separately
-import FilePondPluginImageExifOrientation from 'filepond-plugin-image-exif-orientation';
-import FilePondPluginImagePreview from 'filepond-plugin-image-preview';
 import 'filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css';
-// Register the plugins
-//registerPlugin(FilePondPluginImageExifOrientation, FilePondPluginImagePreview);
 import {connect} from "react-redux";
-import {withStyles} from "@material-ui/core";
 
 function Transition(props) {
     return <Slide direction="up" {...props} />;
 }
 class GalleryRoomUploader extends React.Component {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            // Set initial files
-            files: ['index.html']
-        };
-    }
-    handleInit() {
-        console.log('FilePond instance has initialised', this.pond);
-    }
     render() {
+        const serverConfig = {
+            process: {
+                url: 'http://149.28.202.19:3750/upload',
+                method: 'POST',
+                withCredentials: false,
+                onload: (res)=>{
+                    console.log(JSON.parse(res));
+                },
+            }
+        };
         return (
             <div>
                 {
@@ -59,11 +47,14 @@ class GalleryRoomUploader extends React.Component {
                                     <DialogContentText id="alert-dialog-slide-description">
                                         点击或拖拽上传照片自动生成画廊！
                                     </DialogContentText>
-                                    <FilePond ref={ref => this.pond = ref}
+                                    <FilePond ref={ref => {
+                                        this.pond = ref;
+                                    }}
                                               allowMultiple={true}
                                               maxFiles={9}
-                                              server={'http://149.28.202.19:3750/upload'}
+                                              server={serverConfig}
                                     >
+
                                     </FilePond>
                                 </DialogContent>
                             </Dialog>
@@ -73,6 +64,7 @@ class GalleryRoomUploader extends React.Component {
         );
     }
 }
+
 function mapStateToProps(state) {
     return{
         galleryRoomUploaderActiveReducer: state.galleryRoomUploaderActiveReducer
